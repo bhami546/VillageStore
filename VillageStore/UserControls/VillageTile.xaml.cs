3d140faddb03f24b78ddd2371e7e2cffb8d68268
bhami <bhamireddy.karri@hotmail.com>
2013-09-17 21:27:54 +0530

    Village Store Solution Commit

---
+++ VillageStore/UserControls/VillageTile.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace VillageStore.UserControls
+{
+    public sealed partial class VillageTile : UserControl
+    {
+        public VillageTile()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+       
+
+        
+
+        public ImageSource TileImage
+        {
+            get { return (ImageSource)GetValue(TileImageProperty); }
+            set { SetValue(TileImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileImageProperty =
+            DependencyProperty.Register("TileImage", typeof(ImageSource), typeof(VillageTile), new PropertyMetadata(new BitmapImage()));
+
+
+
+
+        public string TileTitleText
+        {
+            get { return (string)GetValue(TileTitleTextProperty); }
+            set { SetValue(TileTitleTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileTitleText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileTitleTextProperty =
+            DependencyProperty.Register("TileTitleText", typeof(string), typeof(VillageTile), new PropertyMetadata(string.Empty));
+
+        
+
+
+        public string TileText
+        {
+            get { return (string)GetValue(TileTextProperty); }
+            set { SetValue(TileTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileTextProperty =
+            DependencyProperty.Register("TileText", typeof(string), typeof(VillageTile), new PropertyMetadata(string.Empty));
+
+
+
+
+
+        public SolidColorBrush TileBackground
+        {
+            get { return (SolidColorBrush)GetValue(TileBackgroundProperty); }
+            set { SetValue(TileBackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileBackground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileBackgroundProperty =
+            DependencyProperty.Register("TileBackground", typeof(SolidColorBrush), typeof(VillageTile), new PropertyMetadata(new SolidColorBrush(Colors.DarkCyan)));
+
+
+
+
+        public double TileHeight
+        {
+            get { return (double)GetValue(TileHeightProperty); }
+            set { SetValue(TileHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileHeightProperty =
+            DependencyProperty.Register("TileHeight", typeof(double), typeof(VillageTile), new PropertyMetadata(0.0));
+
+
+
+        public double TileWidth
+        {
+            get { return (double)GetValue(TileWidthProperty); }
+            set { SetValue(TileWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileWidthProperty =
+            DependencyProperty.Register("TileWidth", typeof(double), typeof(VillageTile), new PropertyMetadata(0.0));
+
+        
+        private void TileGrid_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TileGrid.Background = TileBackground;
+            //TileImageControl.Source = TileImage;
+            TileTextBlock.Text = TileText;
+            TileHeader.Text = TileTitleText;
+            this.Width = TileWidth;
+            this.Height = TileHeight;
+            
+        }
+    }
+}
---
+++ VillageStore/ViewModels/CitizenDetailsViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.UI.Xaml.Controls.Layout;
+using VillageStore.Common;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using VillageStore.Views;
+
+namespace VillageStore.ViewModels
+{
+    public class CitizenDetailsViewModel : NotificationObject
+    {
+        public CitizenDetailsViewModel()
+        {
+            CitizenMenu = new ObservableCollection<CitizenMenuClass>();
+            CitizenMenu.Add(new CitizenMenuClass("Add Citizen", "ms-appx:///Images/Female1.png",  "#FFA400", "#E78E00",new AddCitizen()));
+            CitizenMenu.Add(new CitizenMenuClass("Search Citizen", "ms-appx:///Images/Male1.png", "#6DA4A3", "#4E7F7D",new SearchCitizen()));
+            CitizenMenu.Add(new CitizenMenuClass("Browse Citizens", "ms-appx:///Images/user.png", "#A45378", "#883F64",new BrowseCitizens()));
+
+        }
+
+        private Orientation orientation = Orientation.Horizontal;
+
+        public Orientation Orientation
+        {
+            get { return orientation; }
+            set { orientation = value; RaisePropertyChanged("Orientation"); }
+        }
+
+        private MinimizedItemsOrientation minOrientation = MinimizedItemsOrientation.Right;
+
+        public MinimizedItemsOrientation MinOrientation
+        {
+            get { return minOrientation; }
+            set { minOrientation = value; RaisePropertyChanged("MinOrientation"); }
+        }
+
+        private ObservableCollection<CitizenMenuClass> _citizenMenu;
+
+        public ObservableCollection<CitizenMenuClass> CitizenMenu
+        {
+            get { return _citizenMenu; }
+            set { _citizenMenu = value; }
+        }
+
+       
+        
+
+    }
+
+    public class CitizenMenuClass
+    {
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string TileColor { get; set; }
+
+        public string HeaderColor { get; set; }
+
+        public Page OutputContent { get; set; }
+
+        public CitizenMenuClass(string name, string image, string color, string headercolor, Page content)
+        {
+            Name = name;
+            Image = image;
+            TileColor = color;
+            HeaderColor = headercolor;
+            OutputContent = content;
+        }
+    }
+}
